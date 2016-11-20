ef3378474fa3cd014f9a9d35a318285a0e7c9862
sierra121314 <sierrag@nevada.unr.edu>
2016-11-19 17:25:38 -0800

    Everything but temp_reward
    Need to figure out how to determine reward

--- Cart_Balance-Code/Cart_Balance-Code.h
+++ Cart_Balance-Code/Cart_Balance-Code.h
@@ -1,13 +1,15 @@
 // Cart_Balance-Code.cpp : Defines the entry point for the console application.
 // Min-Max Evolutionary Controllers Physics Portion
 
-#include "stdafx.h"
+#ifndef _CART_BALANCE_CODE_
+#define _CART_BALANCE_CODE_
+
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
-#include "vector"
+#include <vector>
 #include <iostream>
 
 // Establish boundaries
@@ -16,7 +18,7 @@
 
 // Initialize knowns
 const float g = 9.81f; // m/s^2
-const int n = 1000; // iterations
+const int n = 1; // iterations
 static const double dt = 0.1; //time step
 
 ////Struct has function 
@@ -33,7 +35,6 @@ namespace CB {
 	// forces on the cart
 	// weight of cart
 	struct Cart {
-	public:
 
 		const int Mc = 200; // Mass of the Cart
 		const int y; // Y position of Cart - Irrelevant due to gravity
@@ -61,27 +62,30 @@ namespace CB {
 	};
 
 
-	struct Pendulum {
+	class Pendulum {
 
 		// static variables
 		const double m = 50; // Mass of Pendulum
 		const double L = 10; // Length of the Pendulum
-
+		
 		// collection of states
 		std::vector <Pend_state> pend;
 
 		// Changing variables
 		double torq;
 		double I;
+		double reward; //last reward for the last action
 
 		// public functions
-		void initialize();
-		Pend_state give_state(const int);
-		void get_action();
+		Pendulum();
+		std::vector <double> give_state();
+		void get_action(std::vector <double>);
 		void cycle();
+		std::vector <double> give_reward();
+		void determine_reward();
 	};
 
-	void Pendulum::initialize()
+	void Pendulum::Pendulum()
 	{
 		Pend_state initial;
 		initial.theta = 45 * M_PI / 180;
@@ -102,35 +106,66 @@ namespace CB {
 		// variables
 		Pend_state nextState;
 
-		for (int i = 1; i < n; i++) {
-			// does all necessary calculations, given an action (already set from set_action), to arrive at the next state at the next timestep.
-			//torque to theta dd
-			nextState.theta_dd = -g*cos(pend[i - 1].theta) / (m*L) + torq; //rad/s^2   // define theta_dd with t variable 
-			//thetat_dd to theta_dot
-			nextState.theta_dot = pend[i - 1].theta_dot + nextState.theta_dd*dt;
-			//theta_dot to theta
-			nextState.theta = pend[i - 1].theta + nextState.theta_dot*dt;
-			//theta to xy
-			nextState.Px = L*cos(nextState.theta);
-			nextState.Py = L*sin(nextState.theta);
 
-			std::cout << nextState.theta << "," << nextState.theta_dot << "," << nextState.theta_dd << "," << nextState.Px << "," << nextState.Py << std::endl;
+		// does all necessary calculations, given an action (already set from set_action), to arrive at the next state at the next timestep.
+		//torque to theta dd
+		nextState.theta_dd = -g*cos(pend[i - 1].theta) / (m*L) + torq; //rad/s^2   // define theta_dd with t variable 
+		//thetat_dd to theta_dot
+		nextState.theta_dot = pend[i - 1].theta_dot + nextState.theta_dd*dt;
+		//theta_dot to theta
+		nextState.theta = pend[i - 1].theta + nextState.theta_dot*dt;
+		//theta to xy
+		nextState.Px = L*cos(nextState.theta);
+		nextState.Py = L*sin(nextState.theta);
 
-			pend.push_back(nextState);
+		std::cout << nextState.theta << "," << nextState.theta_dot << "," << nextState.theta_dd << "," << nextState.Px << "," << nextState.Py << std::endl;
 
+		pend.push_back(nextState); //update state
+
+		reward = determine_reward();
+
+		std::ofstream fout;
+		fout.clear();
+		fout.open("positiondata2.csv", std::ofstream::out | std::ofstream::app);
+		for (int i = 0; i < n; i++) { // for i number of iterations 
+									  //calculate xy
+									  //use theta
+									  //output xy
+			fout << pend.give_state(i).Px << "," << pend.give_state(i).Py << "," << dt*i << "," \
+				<< pend.give_state(i).theta << "," << pend.give_state(i).theta_dot << "," \
+				<< pend.give_state(i).theta_dd << std::endl;
 		}
+		fout.close();
+
 	}
 
-	void Pendulum::get_action() { //receives "action vection", which in the first case will just consist of the torque at the joint
+	double determine_reward() {
+		double temp_reward;
+		temp_reward = 0.0;
+		return temp_reward;
+	}
+
+	void Pendulum::get_action(std::vector <double> in_action) { //receives "action vection", which in the first case will just consist of the torque at the joint
 
 		//name vector
 		// torq=t@0
-		torq = 0;
+		torq = in_action.at(0); 
+		cycle();
 	}
 
-	Pend_state Pendulum::give_state(const int i) {
+	std::vector <double> Pendulum::give_state() {
 		//gives the state of the pendulum at the given timestep
-		return pend[i];
+		std::vector <double> temp_state;
+		temp_state.push_back(pend.at(pend.end()).theta);
+		temp_state.push_back(pend.at(pend.end()).theta_dot);
+		return temp_state;
 	}
+	std::vector <double> Pendulum::give_reward() {
+		std::vector <double> temp_reward;
+		temp_state.push_back(reward);
 
+		return temp_reward;
+	}
 }
+
+#endif // !
